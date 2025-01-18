d3357c2f3525ef21405c213a06194c81457a4dc7
Mimoun El Yazidi <87827134+min45@users.noreply.github.com>
2025-01-18 11:54:49 +0000

    todo list flask mimoun

--- src/app.py
+++ src/app.py
@@ -1,45 +1,31 @@
 from flask import Flask, jsonify, request
 
+
 app = Flask(__name__)
 
-# A list to store todo items, each represented as a dictionary with "label" and "done" fields
-todos = [
-    { "label": "My first task", "done": False },
-    { "label": "My second task", "done": False }
-]
 
-# Route to retrieve the list of todos
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    # Convert the todos list to a JSON response format
-    json_text = jsonify(todos)
+    return jsonify(todos)
 
-    # Return the JSON response to the front-end
-    return json_text
 
-# Route to add a new todo item
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    # Get JSON data from the request body
     request_body = request.json
-    
-    # Append the new todo item to the todos list
+    print("Incoming ....", request_body)
     todos.append(request_body)
-    
-    # Return the updated todos list as a JSON response
-    json_text = jsonify(todos)
-    return json_text
+    return jsonify(todos)
 
-# Route to delete a todo item at a specific position
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    # Remove the item at the specified position in the todos list
-    todos.pop(position)
-    
-    # Return the updated todos list as a JSON response
-    json_text = jsonify(todos)
-    return json_text
+    print("La posicion es:", position)
+    del todos[position]
+    return jsonify(todos)
 
-# These two lines should always be at the end of your app.py file to start the Flask application
+
+some_data = { "name": "Mimoun", "lastname": "El Yazidi" }
+todos = [{ "label": "My first task", "done": False },]
+
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
